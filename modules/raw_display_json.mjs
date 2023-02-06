f83a3c8d3533f2ad6f9e8c7fb1432f0bd4fb6fbd
konradusko <boleslawski.konrad@gmail.com>
2023-02-06 19:04:54 +0100

    Working with templates

--- modules/raw_display_json.mjs
+++ modules/raw_display_json.mjs
@@ -1,9 +1,10 @@
+import { Module_config } from "./config.mjs"
 export const get_template = (jsonData)=>{
     return `<html>
     <body>
     <pre>
     <code>
-        ${JSON.stringify(jsonData, null, 6)}
+        ${JSON.stringify({data:jsonData,template:Module_config.getConfig().template_data}, null, 6)}
     </code>
     </pre>
     </body>
